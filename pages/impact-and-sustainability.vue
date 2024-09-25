0ce02d95f739171cee57858bcac70acbfa97d486
Tochukwu Franklin Ene <teec3000@gmail.com>
2024-09-25 15:05:20 +0100

    working on sidenav

--- pages/impact-and-sustainability.vue
+++ pages/impact-and-sustainability.vue
@@ -1,8 +1,8 @@
 <template>
-<section class="bg-[url('/assets/images/impact2.png')] bg-center bg-cover bg-clip-border bg-no-repeat text-center text-white items-center m-auto justify-center  space-y-0 flex flex-col py-[5%]">
+<section class="bg-[url('/assets/images/sm-impact2.png')] md:bg-[url('/assets/images/impact.png')] bg-center bg-cover bg-clip-border bg-no-repeat text-center text-white items-center m-auto justify-center  space-y-0 flex flex-col py-[20%] md:py-[5%]">
     <h3 class="text-[24px] lg:text-[64px] font-ti">Impact and Sustainability </h3>
 </section>
-<section class=" bg-white flex flex-col md:flex-row pt-[2%]">
+<section class=" bg-white flex flex-col md:flex-row space-y-[4%] md:pt-[2%]">
     <div class=" items-center space-y-4 pt-[4%] px-[6%] lg:w-[55%]">
         <h2 class="font-bold text-[20px] lg:text-[44px] leading-[50px] font-ti">Our Commitment to Sustainability</h2>
         <p class="font-ar text-[14px] lg:text-[24px] text-black font-normal lg:leading-[45px]">
@@ -14,16 +14,16 @@
     </div>
         <img src="/assets/images/earth.png"  class="lg:w-[45%]"/>
 </section>
-<section class="bg-[url('/assets/images/impact2.png')] bg-center bg-cover bg-clip-border bg-no-repeat text-center text-white items-center m-auto justify-center  space-y-0 flex flex-col py-[1%]">
+<section class="bg-[url('/assets/images/impact2.png')] bg-center bg-cover bg-clip-border bg-no-repeat text-center text-white items-center m-auto justify-center  space-y-0 flex flex-col py-[1%] hidden md:block">
 </section>
 <section class=" md:px-[2%] lg:px-[2%] xl:px-[4%] bg-white">
     <h2 class="font-bold text-[24px] md:text-[48px] md:leading-[50px] font-ti text-center p-4 py-[8%]">Our Impact Story</h2>
-    <div class="flex flex-col lg:flex-row items-center justify-center ">
+    <div class="flex flex-col lg:flex-row items-center justify-center space-y-4">
         <div class="lg:w-[55%]">
             <mapComponent />
         </div>
-        <div class="lg:w-[45%] border-[10px] border-theme-gray md:p-10 space-y-[5%] float-center h-fit">
-            <div class="grid md:grid-cols-2 gap-[4%] md:gap-14 py-4">
+        <div class="lg:w-[45%] border-[10px] border-theme-gray p-3 md:p-10 md:space-y-[5%] float-center h-fit">
+            <div class="px-[10px] sm:flex sm:flex-col md:grid md:grid-cols-2 gap-[6%] md:gap-14 space-y-[5%] md:space-y-0">
                 <div class="flex gap-4 h-fit items-center">
                     <img src="/assets/images/handcase.svg" />
                     <div class="font-in text-black font-semibold leading-8">
@@ -99,15 +99,15 @@
     <div class="relative md:text-[32px] font-ar font-oblique italic  w-[70%] mx-auto py-[5%] text-black">
         
         <p class="md:text-justify">
-            <span class="absolute md:-left-7 md:text-[64px] leading-none text-black">&ldquo;</span>
-            Our success with these pilot institutions is just the beginning. We are poised to onboard 10 more partners this year, expanding our impact and ensuring that every region in Nigeria benefits from inclusive and sustainable financing.<span class="absolute bottom-0  text-[64px] leading-none text-black">&rdquo;</span>
+            <span class="absolute -left-4 md:-left-7 text-[34px] md:text-[64px] leading-none text-black ">&ldquo;</span>
+            Our success with these pilot institutions is just the beginning. We are poised to onboard 10 more partners this year, expanding our impact and ensuring that every region in Nigeria benefits from inclusive and sustainable financing.<span class="absolute bottom-0  text-[34px] md:text-[64px] leading-none text-black">&rdquo;</span>
         </p>
         
     </div>
     
 
 </section>
-<section class="bg-[url('/assets/images/impact2.png')] bg-center bg-cover bg-clip-border bg-no-repeat text-center text-white items-center m-auto justify-center  space-y-0 flex flex-col py-[5%]">
+<section class="bg-[url('/assets/images/sm-impact2.png')] md:bg-[url('/assets/images/impact2.png')] bg-center bg-cover bg-clip-border bg-no-repeat text-center text-white items-center m-auto justify-center  space-y-0 flex flex-col py-[7%] md:py-[5%]">
 </section>
 </template>
 
